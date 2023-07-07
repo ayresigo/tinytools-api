f83638d59ee001f7b62621e59c85d6a8a27d1ff7
Igor Frazão <38304023+ayresigo@users.noreply.github.com>
2023-07-07 17:29:27 -0300

    adding some logs

--- src/modules/webhook/webhook.controller.ts
+++ src/modules/webhook/webhook.controller.ts
@@ -10,18 +10,24 @@ export class WebhookController {
   @HttpCode(200)
   @Post('goldtech')
   async receiveGoldtechWebhook(@Body() body: object) {
-    return this.webhookService.receiveCustomWebhook(body, 'goldtech');
+    var response = await this.webhookService.receiveCustomWebhook(body, 'goldtech');
+    console.log(response);
+    return response;
   }
 
   @HttpCode(200)
   @Post('megatech')
   async receiveMegatechWebhook(@Body() body: object) {
-    return this.webhookService.receiveCustomWebhook(body, 'megatech');
+    var response = await this.webhookService.receiveCustomWebhook(body, 'megatech');
+    console.log(response);
+    return response;
   }
 
   @HttpCode(200)
   @Get('startRoutine')
   async start_routine(@Query('id') id: string) {
-    return this.webhookService.testWebhook(id);
+    var response = await this.webhookService.testWebhook(id);
+    console.log(response);
+    return response;
   }
 }
