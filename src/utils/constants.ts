4e53f6f5db75816b81bd420f804a49738a6852e6
Daniel Yoshimatsu <danielyoshipele@gmail.com>
2024-07-26 20:00:05 -0300

    .

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,11 @@ export const constants = {
   SCRAPED_BASE_URL: 'https://erp.tiny.com.br/',
   SCRAPED_INVOICE_ENDPOINT: 'services/notas.fiscais.server.php',
   SCRAPED_LOGIN_ENDPOINT: 'services/reforma.sistema.server.php',
+<<<<<<< HEAD
   SCRAPED_FRONT_VERSTION: '3.65.27',
+=======
+  SCRAPED_FRONT_VERSTION: '3.67.03',
+>>>>>>> dc43b25 (.)
 
   AUTH_ERROR_PREFIX: 'alert(',
   INVOICE_ITEM_PREFIX: 'setarArrayItens(',
